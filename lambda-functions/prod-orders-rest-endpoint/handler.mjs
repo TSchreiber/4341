42a4ae144c549ad3c042adc00a582bd4984f380d
TSchreiber <24798607+TSchreiber@users.noreply.github.com>
2023-11-01 15:33:17 -0500

    added input validation for POST /orders endpoint

--- lambda-functions/prod-orders-rest-endpoint/handler.mjs
+++ lambda-functions/prod-orders-rest-endpoint/handler.mjs
@@ -26,32 +26,83 @@ return async (event, context) => {
                 break;
             
             case "POST":
-                let order = JSON.parse(event.body);
-                order.OrderId = crypto.randomUUID();
                 let id_token_str = event.headers.authorization.split(" ")[1];
                 let id_token = JSON.parse(Buffer.from(id_token_str.split('.')[1], 'base64').toString());
-                order.user_id = id_token["cognito:username"];
+                if (!id_token || !id_token.sub || !id_token.email_verified) {
+                    statusCode = 403;
+                    break;
+                }
+
+                let orderRequest = JSON.parse(event.body);
+                let order = {};
+                order.OrderId = crypto.randomUUID();
+                order.user_id = id_token.sub;
                 order.timestamp = Date.now();
                 
                 let promises = [];
-                for (let PLU of order.items) {
-                    promises.push(new Promise(res => {
-                        db.getProductByPLU(PLU)
-                        .then(item => {
+                for (let PLU of orderRequest.items) {
+                    promises.push(new Promise(async (res,rej) => {
+                        try {
+                            let item = await db.getProductByPLU(PLU);
                             res({"PLU": item.PLU, "price": item.price});
-                        })
+                        } catch (err) {
+                            rej(err);
+                        }
                     }))
                 }
                 order.items = await Promise.all(promises)
+
+                // Basic card validation - these are the minimum requirements for a 
+                // a card to possibly be valid, but does not fully verify that 
+                // the card is valid
+                let paymentInfo = orderRequest.paymentInfo;
+                if (!paymentInfo) {
+                    statusCode = 400;
+                    break;
+                }
+                let PAN = paymentInfo.cardNumber;
+                let cvc = paymentInfo.securityCode;
+                let nameOnCard = paymentInfo.nameOnCard;
+                let expiration = paymentInfo.expiration;
+                if (!PAN || !cvc || !nameOnCard || !expiration) {
+                    statusCode = 400;
+                    break;
+                }
+
+                if (PAN.length < 8 || PAN.length > 19) {
+                    statusCode = 400;
+                    break;
+                }
+                let luhnSum = PAN.split("")
+                    .reverse()
+                    .map(s => parseInt(s))
+                    .map((x,i) => i%2 == 0 ? x : x*2)
+                    .reduce((sum,x) => sum+x, 0);
+                if (luhnSum % 10 != 0) {
+                    statusCode = 400;
+                    break;
+                }
+
+                let [_,month,year] = 
+                    expiration.match("^(\\d{2})/(\\d{2})$") ||
+                    expiration.match("^(\\d{2})/(\\d{4})$");
+                // Did not use,
+                // > `new Date(year, monthIndex)`
+                // Because it considers year 20 to be 1920
+                if (new Date(`${month}/01/${year}`) < new Date()) {
+                    statusCode = 400;
+                    break;
+                }
                 
                 // This is were the card information would be verified and the
                 // payment process would be started with some payment api like stripe
                 
-                // Then remove the card information
-                order.paymentInfo.cardNumber = order.paymentInfo.cardNumber.substring(12);
-                delete order.paymentInfo.securityCode;
-                delete order.paymentInfo.expiration;
-                delete order.paymentInfo.nameOnCard;
+                order.paymentInfo = {};
+                // Only save the last four digits of the PAN to meet PCI Data Security Standard
+                order.paymentInfo.cardNumber = PAN.substring(PAN.length - 4);
+
+                order.shippingAddress = orderRequest.shippingAddress;
+                order.billingAddress = orderRequest.billingAddress;
                 
                 await db.insertOrder(order);
 
