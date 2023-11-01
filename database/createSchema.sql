b39fd0f6974941c1e0ee16700a5a101db0092052
TSchreiber <24798607+TSchreiber@users.noreply.github.com>
2023-11-01 15:21:59 -0500

    added tests for /orders endpoint

--- database/createSchema.sql
+++ database/createSchema.sql
@@ -46,3 +46,13 @@ insert into Products values
 (982038352481, "Premium Organic Coffee Beans", "Enjoy the rich and aromatic flavors of our ethically sourced, 100% organic coffee beans.", "https://utd-cs4389-f2023-team1.s3.us-east-2.amazonaws.com/product-images/Premium_Organic_Coffee_Beans.png", 1499),
 (921363448554, "CapturePro DSLR Camera", "A professional-grade DSLR camera with a high-resolution sensor, 4K video recording, and advanced shooting modes.", "https://utd-cs4389-f2023-team1.s3.us-east-2.amazonaws.com/product-images/CapturePro_DSLR_Camera.png", 149999),
 (931061209310, "SpeedMax Running Shoes", "Lightweight and breathable running shoes designed for maximum speed and comfort during your workouts.", "https://utd-cs4389-f2023-team1.s3.us-east-2.amazonaws.com/product-images/SpeedMax_Running_Shoes.png", 8999);
+
+insert into Address (firstName, lastName, addressLine1, city, state, zip) values 
+("John","Doe","800 W Campbell Rd","Richardson","TX","75080"),
+("John","Doe","800 W Campbell Rd","Richardson","TX","75080");
+
+insert into Orders (OrderId,user_id,shippingAddressId,billingAddressId,timestamp,cardNumber) values 
+("21636e24-d65c-4eb8-9477-50c2c4f5d735", "e586c8b1-59aa-4c05-bfcc-58a4e18e5902", 2, 1, 1698861040084, "1111");
+
+insert into OrderItem (OrderId,PLU,unitPrice) values
+("21636e24-d65c-4eb8-9477-50c2c4f5d735", 982038352481, 1499);
--- test/orders_endpoint.mjs
+++ test/orders_endpoint.mjs
@@ -1,57 +1,335 @@
-const test_id_token = "eyJraWQiOiJnMVNpYjNpS05Tb1ZXYlZcL1Z5Ykl5cFwvbjJjYnNOaFRrdTJxcXRiUEhySTA9IiwiYWxnIjoiUlMyNTYifQ.eyJhdF9oYXNoIjoiSWpqVkZad1UyenpXMUVDa0E2cHFEQSIsInN1YiI6ImU1ODZjOGIxLTU5YWEtNGMwNS1iZmNjLTU4YTRlMThlNTkwMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl9XcWRoZEdQSXIiLCJjb2duaXRvOnVzZXJuYW1lIjoiZTU4NmM4YjEtNTlhYS00YzA1LWJmY2MtNThhNGUxOGU1OTAyIiwib3JpZ2luX2p0aSI6IjhhNDdjNGY4LWEyYzUtNDJhMS1hMDE2LWQ3ZTBmOWIwMzBkMyIsImF1ZCI6IjRwdHZsb2NlZ2UwajNpcmV1a29lODNqbTF1IiwiZXZlbnRfaWQiOiIyMDNjOGE5Yi1mN2NhLTQ1OTItODI3Yy1mMWRjYWI5NjZlNTkiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY5ODUyMzM5OCwiZXhwIjoxNjk4NTI2OTk4LCJpYXQiOjE2OTg1MjMzOTgsImp0aSI6IjAxZGJjNjgyLWExZGEtNDcwMS1iZThhLWUwYjkyMzAyMTgxYiIsImVtYWlsIjoidGRzMTgwMDAxQHV0ZGFsbGFzLmVkdSJ9.i-NtNjmjGSsyHd9DCOdYKJGZ4R8G7K-3w6rvaVNU9FaCG6HwdoF5TNuvuUPcBlgK_VPJXZOSjybn2fEvuemheD6KOKLznRGXj1QUJqakcCtG-Nh2hpSSPvBwM61J112P-oqHSVUjfLK4JI-fcnCdfb6KzqK5r_3W7GojaWMJRnaAU3lXmPnAKznqtMVxDJnZDBVCJ3lPSbXdrufdXwFdWT-bRM3djgvt33ZO_5J9nOuD74uJBcl-2GZJRp23HOgGGC1Gqr6wu2K6Nw6c_jaKcKKjMItfp_ziVYIdSCHoOUl7tALuXfaaFp7CQSXf7gq0PWzuDmVJv2ZpA4mgcc5cqg";
-
 import db from "../server/db.mjs"
 import { createHandler as createOrderHandler } from "../lambda-functions/prod-orders-rest-endpoint/handler.mjs";
 const handleOrderRequest = createOrderHandler(db);
 import { createHandler as createProductHandler } from "../lambda-functions/prod-products-rest-endpoint/handler.mjs";
 const handleProductRequest = createProductHandler(db);
 import assert from "assert";
+import fs from "fs";
+import { test_data } from "./test_data.mjs"
 
-describe('orders endpoint', () => {
-    describe('posting an order', () => {
+describe('Sending a POST request to the /orders endpoint', () => {
+    describe('with a valid request', () => {
         it("should return the order id of the created order", async () => {
             let res = await handleOrderRequest({
                 resource: "/orders",
                 httpMethod: "POST",
                 headers: {
-                    authorization: "Bearer " + test_id_token
+                    authorization: "Bearer " + test_data.valid_id_tokens[0]
                 },
-                body: JSON.stringify({
-                    shippingAddress: {
-                        firstName: 'John',
-                        middleName: '',
-                        lastName: 'Doe',
-                        mailingAddress1: '800 W Campbell Rd',
-                        mailingAddress2: '',
-                        city: 'Richardson',
-                        state: 'TX',
-                        zip: '75080',
-                        urbanization: ''
-                    },
-                    paymentInfo: {
-                        nameOnCard: 'John Doe',
-                        cardNumber: '4111111111111111',
-                        securityCode: '111',
-                        expiration: '01/2030'
-                    },
-                    billingAddress: {
-                        firstName: 'John',
-                        middleName: '',
-                        lastName: 'Doe',
-                        mailingAddress1: '800 W Campbell Rd',
-                        mailingAddress2: '',
-                        city: 'Richardson',
-                        state: 'TX',
-                        zip: '75080',
-                        urbanization: ''
-                    },
-                    items: [ '982038352481' ]
-                })
+                body: JSON.stringify(test_data.valid_orders[0])
             });
 
             assert(res.statusCode == 200);
             assert(JSON.parse(res.body));
         });
     });
+
+    describe("with an invalid authentication token should return a \"403 - Forbidden\" status", () => {
+        it("when the user id is not provided", async () => {
+            let res = await handleOrderRequest({
+                resource: "/orders",
+                httpMethod: "POST",
+                headers: {
+                    authorization: "Bearer " + test_data.invalid_id_tokens.user_id_not_provided
+                },
+                body: JSON.stringify(test_data.valid_orders[0])
+            });
+
+            if (res.statusCode != 403) {
+                throw new Error(`Expected "403" status code, but got "${res.statusCode}"`);
+            }
+        });
+
+        it("when the user's email is not verified", async () => {
+            let res = await handleOrderRequest({
+                resource: "/orders",
+                httpMethod: "POST",
+                headers: {
+                    authorization: "Bearer " + test_data.invalid_id_tokens.email_not_verified
+                },
+                body: JSON.stringify(test_data.valid_orders[0])
+            });
+
+            if (res.statusCode != 403) {
+                throw new Error(`Expected "403" status code, but got "${res.statusCode}"`);
+            }
+        });
+    });
+
+    describe("with an invalid order request should return a \"400 - Bad Request\" status", () => {
+        it("when either address does not contain \"name\", \"mailingAddress1\", \"city\", and \"zip\"", async () => {
+            for (let address of test_data.invalid_addresses) {
+                let res = await handleOrderRequest({
+                    resource: "/orders",
+                    httpMethod: "POST",
+                    headers: {
+                        authorization: "Bearer " + test_data.valid_id_tokens[0]
+                    },
+                    body: JSON.stringify({
+                        shippingAddress: address,
+                        billingAddress: test_data.valid_addresses[0],
+                        paymentInfo: test_data.valid_payment_methods[0],
+                        items: test_data.valid_item_PLUs
+                    })
+                });
+                if (res.statusCode != 400) {
+                    throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+                }
+            }
+        });
+
+        it("when the items array is \"undefined\", \"null\", or empty", async () => {
+            let res = await handleOrderRequest({
+                resource: "/orders",
+                httpMethod: "POST",
+                headers: {
+                    authorization: "Bearer " + test_data.valid_id_tokens[0]
+                },
+                body: JSON.stringify({
+                    shippingAddress: test_data.valid_addresses[0],
+                    billingAddress: test_data.valid_addresses[0],
+                    paymentInfo: test_data.valid_payment_methods[0],
+                    items: test_data.invalid_item_PLUs
+                })
+            });
+            if (res.statusCode != 400) {
+                throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+            }
+        });
+
+        it("when paymentInfo does not contain \"cardNumber\", \"CVC\", \"nameOnCard\", and \"cardExpiration\"", async () => {
+            for (let paymentMethod of test_data.invalid_payment_methods.missing_information) {
+                let res = await handleOrderRequest({
+                    resource: "/orders",
+                    httpMethod: "POST",
+                    headers: {
+                        authorization: "Bearer " + test_data.valid_id_tokens[0]
+                    },
+                    body: JSON.stringify({
+                        shippingAddress: test_data.valid_addresses[0],
+                        billingAddress: test_data.valid_addresses[0],
+                        paymentInfo: paymentMethod,
+                        items: test_data.valid_item_PLUs
+                    })
+                });
+                if (res.statusCode != 400) {
+                    throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+                }
+            };
+        });
+
+        it("when cardNumber has less than 8 digits or more than 19 digits", async () => {
+            for (let paymentMethod of test_data.invalid_payment_methods.incorrect_number_length) {
+                let res = await handleOrderRequest({
+                    resource: "/orders",
+                    httpMethod: "POST",
+                    headers: {
+                        authorization: "Bearer " + test_data.valid_id_tokens[0]
+                    },
+                    body: JSON.stringify({
+                        shippingAddress: test_data.valid_addresses[0],
+                        billingAddress: test_data.valid_addresses[0],
+                        paymentInfo: paymentMethod,
+                        items: test_data.valid_item_PLUs
+                    })
+                });
+                if (res.statusCode != 400) {
+                    throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+                }
+            };
+        });
+
+        it("when cardNumber does not pass the Luhn algorithm check", async () => {
+            for (let paymentMethod of test_data.invalid_payment_methods.invalid_number) {
+                let res = await handleOrderRequest({
+                    resource: "/orders",
+                    httpMethod: "POST",
+                    headers: {
+                        authorization: "Bearer " + test_data.valid_id_tokens[0]
+                    },
+                    body: JSON.stringify({
+                        shippingAddress: test_data.valid_addresses[0],
+                        billingAddress: test_data.valid_addresses[0],
+                        paymentInfo: paymentMethod,
+                        items: test_data.valid_item_PLUs
+                    })
+                });
+                if (res.statusCode != 400) {
+                    throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+                }
+            };
+        });
+
+        it("when cardExpiration is not in the format \"MM/YY\", or \"MM/YYYY\"", async () => {
+            for (let paymentMethod of test_data.invalid_payment_methods.invalid_expiration_format) {
+                let res = await handleOrderRequest({
+                    resource: "/orders",
+                    httpMethod: "POST",
+                    headers: {
+                        authorization: "Bearer " + test_data.valid_id_tokens[0]
+                    },
+                    body: JSON.stringify({
+                        shippingAddress: test_data.valid_addresses[0],
+                        billingAddress: test_data.valid_addresses[0],
+                        paymentInfo: paymentMethod,
+                        items: test_data.valid_item_PLUs
+                    })
+                });
+                if (res.statusCode != 400) {
+                    throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+                }
+            }
+        });
+
+        it("when cardExpiration is in the past", async () => {
+            for (let paymentMethod of test_data.invalid_payment_methods.expired_card) {
+                let res = await handleOrderRequest({
+                    resource: "/orders",
+                    httpMethod: "POST",
+                    headers: {
+                        authorization: "Bearer " + test_data.valid_id_tokens[0]
+                    },
+                    body: JSON.stringify({
+                        shippingAddress: test_data.valid_addresses[0],
+                        billingAddress: test_data.valid_addresses[0],
+                        paymentInfo: paymentMethod,
+                        items: test_data.valid_item_PLUs
+                    })
+                });
+                if (res.statusCode != 400) {
+                    throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+                }
+            }
+        });
+
+        it("when any item is not in the products catalog", async () => {
+            let res = await handleOrderRequest({
+                resource: "/orders",
+                httpMethod: "POST",
+                headers: {
+                    authorization: "Bearer " + test_data.valid_id_tokens[0]
+                },
+                body: JSON.stringify({
+                    shippingAddress: test_data.valid_addresses[0],
+                    billingAddress: test_data.valid_addresses[0],
+                    paymentInfo: test_data.valid_payment_methods[0],
+                    items: test_data.invalid_item_PLUs
+                })
+            });
+            if (res.statusCode != 400) {
+                throw new Error(`Expected "400" status code, but got "${res.statusCode}"`);
+            }
+        });
+    });
 });
 
+describe("Sending a GET request to the /orders endpoint", () => {
+    it("should return a \"403 - Forbidden\" status when the user id is not provided in the id token", async () => {
+        let res = await handleOrderRequest({
+            resource: "/orders",
+            httpMethod: "GET",
+            headers: {
+                authorization: "Bearer " + test_data.invalid_id_tokens.user_id_not_provided
+            }
+        });
+
+        if (res.statusCode != 403) {
+            throw new Error(`Expected "403" status code, but got "${res.statusCode}"`);
+        }
+    });
+
+    it ("should return all orders created by the user", async () => {
+        let res = await handleOrderRequest({
+            resource: "/orders",
+            httpMethod: "GET",
+            headers: {
+                authorization: "Bearer " + test_data.valid_id_tokens[0]
+            }
+        });
+        
+        if (res.statusCode != 200) {
+            throw new Error(`Expected "200" status code, but got "${res.statusCode}"`);
+        }
+        let orders = JSON.parse(res.body);
+        if (orders.length <= 0) {
+            throw new Error(`Returned empty array`);
+        } else if (!orders[0].OrderId) {
+            throw new Error(`Did not return the correct orders, ${res.body}`);
+        }
+    });
+
+    it ("should not return any orders created by another user", async () => {
+        let res = await handleOrderRequest({
+            resource: "/orders",
+            httpMethod: "GET",
+            headers: {
+                authorization: "Bearer " + test_data.valid_id_tokens[0]
+            }
+        });
+        
+        if (res.statusCode != 200) {
+            throw new Error(`Expected "200" status code, but got "${res.statusCode}"`);
+        }
+        let orders = JSON.parse(res.body);
+        for (let order of orders) {
+            if (order.user_id != "e586c8b1-59aa-4c05-bfcc-58a4e18e5902") {
+                throw new Error(`Recieved an order from another user, ${res.body}`);
+            }
+        }
+    });
+});
+
+describe("Sending a GET request to the /orders/{OrderId+} endpoint", () => {
+    it("should return a \"403 - Forbidden\" status when the user id is not provided in the id token", async () => {
+        let res = await handleOrderRequest({
+            resource: "/orders/{OrderId+}",
+            httpMethod: "GET",
+            headers: {
+                authorization: "Bearer " + test_data.invalid_id_tokens.user_id_not_provided
+            },
+            pathParameters: {
+                OrderId: test_data.valid_order_ids[0]
+            }
+        });
+
+        if (res.statusCode != 403) {
+            throw new Error(`Expected "403" status code, but got "${res.statusCode}"`);
+        }
+    });
+    it("should return a \"403 - Forbidden\" status when the user is not the owner of the order", async () => {
+        let res = await handleOrderRequest({
+            resource: "/orders{OrderId+}",
+            httpMethod: "GET",
+            headers: {
+                authorization: "Bearer " + test_data.valid_id_tokens[1]
+            },
+            pathParameters: {
+                OrderId: test_data.valid_order_ids[0]
+            }
+        });
+
+        if (res.statusCode != 403) {
+            throw new Error(`Expected "403" status code, but got "${res.statusCode}"`);
+        }
+    });
+
+    it ("should return the specified order when the request is valid", async () => {
+        let res = await handleOrderRequest({
+            resource: "/orders/{OrderId+}",
+            httpMethod: "GET",
+            headers: {
+                authorization: "Bearer " + test_data.valid_id_tokens[0]
+            },
+            pathParameters: {
+                OrderId: "21636e24-d65c-4eb8-9477-50c2c4f5d735"
+            }
+        });
+
+        if (res.statusCode != 200) {
+            throw new Error(`Expected "200" status code, but got "${res.statusCode}"`);
+        }
+        if (JSON.parse(res.body).OrderId != "21636e24-d65c-4eb8-9477-50c2c4f5d735") {
+            throw new Error(`Returned incorrect order, ${res.body}`);
+        }
+    });
+});
